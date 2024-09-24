6bbc2ab8a98c30ca98aa57d65eb6ceac41746a67
AZLINA MAT ZAIN <quakeroat.dew@gmail.com>
2024-09-24 13:00:57 +0800

    first commit

---
+++ mysupportapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Feedback)
+admin.site.register(Organization)
+admin.site.register(Agency)
+admin.site.register(CorporateInfo)
+admin.site.register(ShareCapital)
+admin.site.register(Directors)
+admin.site.register(Shareholders)
+admin.site.register(FinancialInfo)
